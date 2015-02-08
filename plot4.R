f19232ea8209cf70644fb208467df59f5602f382
Bill Ebert <ebertwj@netscape.net>
2015-02-08 08:13:53 -0600

    added plot 4 and modified previous images to complete project

---
+++ plot4.R
@@ -0,0 +1,54 @@
+## read header data from first line of data file
+header <- read.table("C:/Users/bill/downloads/exdata_data_household_power_consumption/household_power_consumption.txt", nrows = 1, header = FALSE, sep = ";", na.strings = c("?"), stringsAsFactors = FALSE)
+
+## read rest of data into data frame
+data <- read.table("C:/Users/bill/downloads/exdata_data_household_power_consumption/household_power_consumption.txt", skip = 2, header = FALSE, sep = ";", na.strings = c("?"), stringsAsFactors = FALSE)
+
+## change column names to names read from header
+colnames(data) <- unlist(header)
+
+## change data in Date column to date objects
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+## change data in Time column to date objects
+data$Time <- strptime(data$Time, "%H:%M:%S")
+
+## format time data in Time column to show time only
+data$Time <- strftime(data$Time, "%H:%M:%S")
+
+## set value for start of subset of data
+startDate <- as.Date(c("01/02/2007"), "%d/%m/%Y")
+
+## set value for end of subset of data
+endDate <- as.Date(c("02/02/2007"), "%d/%m/%Y")
+
+## subset data to dates of 1-2-2007 and 2-2-2007 only
+dataSubset <- subset(data, Date == startDate | Date == endDate)
+
+## combine date and time columns into single date object in Date column
+dataSubset$Date <- as.POSIXct(paste(dataSubset$Date, dataSubset$Time), format="%Y-%m-%d %H:%M:%S")
+
+## set margins for plot
+par(mfrow = c(2, 2), mar = c(4, 4, 3, 1))
+
+
+
+## set plots with labels
+with(dataSubset, {
+        plot(Date, Global_active_power, xlab = "", ylab = "Global Active Power", cex.axis = 0.6, cex.lab = 0.5, type = "n")
+        lines(dataSubset$Date, dataSubset$Global_active_power, type = "l")
+
+        plot(Date, Voltage, xlab = "datetime", ylab ="Voltage", cex.axis = 0.6, cex.lab = 0.5, type = "n")
+        lines(dataSubset$Date, dataSubset$Voltage, type = "l")
+
+        plot(Date, Sub_metering_1, xlab = "", ylab = "Energy sub metering", cex.axis = 0.6, cex.lab = 0.65, type = "n")
+
+        ## add lines for data
+        lines(dataSubset$Date, dataSubset$Sub_metering_1, type = "l")
+        lines(dataSubset$Date, dataSubset$Sub_metering_2, col = "red")
+        lines(dataSubset$Date, dataSubset$Sub_metering_3, col = "blue")
+        legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = c(1, 1, 1), bty = "n", cex = 0.5)
+        
+        plot(Date, Global_reactive_power, cex.axis = 0.6, cex.lab = 0.5, type = "n")
+        lines(dataSubset$Date, dataSubset$Global_reactive_power, type = "l")
+})
